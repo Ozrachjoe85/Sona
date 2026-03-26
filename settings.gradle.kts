177028bf7c14c92f7a4fabe8ec745d493278e826
Ozrachjoe85 <ozrachjoe85@gmail.com>
2026-03-26 10:44:25 -0600

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,9 +10,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
     }
 }
-
 rootProject.name = "SONA"
 include(":app")
